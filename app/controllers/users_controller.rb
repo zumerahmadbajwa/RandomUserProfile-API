ce1e8d4f9802c951f8c854ae47f6b160274a567b
Zumer Ahmad <macbookpro@Macbooks-MBP-2.lan>
2024-09-05 16:52:05 +0500

    User-Friendly Error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = RandomUserService.fetch_random_user
+    user = RandomUserService.fetch_random_user
+    if user.is_a?(Hash) && user[:error]
+      flash[:alert] = user[:error]
+      @user = nil
+    else
+      @user = user
+    end
   end
 end
--- app/services/random_user_service.rb
+++ app/services/random_user_service.rb
@@ -4,16 +4,24 @@ class RandomUserService
   API_URL = 'https://randomuser.me/api/'
 
   def self.fetch_random_user
-    response = HTTP.get(API_URL)
-    user = JSON.parse(response.body)['results'].first
-    {
-      first_name: user['name']['first'],
-      last_name: user['name']['last'],
-      email: user['email'],
-      gender: user['gender'],
-      city: user['location']['city'],
-      country: user['location']['country'],
-      picture: user['picture']['large']
-    }
+    begin
+      response = HTTP.get(API_URL)
+      user = JSON.parse(response.body)['results'].first
+       if user
+        {
+          first_name: user['name']['first'],
+          last_name: user['name']['last'],
+          email: user['email'],
+          gender: user['gender'],
+          city: user['location']['city'],
+          country: user['location']['country'],
+          picture: user['picture']['large']
+        }
+      else
+        { error: 'No user data available' }
+      end
+    rescue HTTP::Error => e
+      { error: "Error! #{e.message}" }
+    end
   end
 end
