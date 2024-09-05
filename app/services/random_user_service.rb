189dffb2144ba74648b39152cfdc8e02a88ced74
Zumer Ahmad <macbookpro@Macbooks-MBP-2.lan>
2024-09-05 13:46:40 +0500

    JSON parsing and Formating the Responses

--- app/services/random_user_service.rb
+++ app/services/random_user_service.rb
@@ -5,6 +5,15 @@ class RandomUserService
 
   def self.fetch_random_user
     response = HTTP.get(API_URL)
-    JSON.parse(response.body)['results']
+    user = JSON.parse(response.body)['results'].first
+    {
+      first_name: user['name']['first'],
+      last_name: user['name']['last'],
+      email: user['email'],
+      gender: user['gender'],
+      city: user['location']['city'],
+      country: user['location']['country'],
+      picture: user['picture']['large']
+    }
   end
 end
